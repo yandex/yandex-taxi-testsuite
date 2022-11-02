eee5e9da467a2e69e3cfcb503b30b1c0d068fe64
Aleksandr Lazarev <lexlazarev@userver.tech>
2022-11-02 03:34:05 +0300

    add timeout to redis-cluster script

--- tests/databases/redis/test_cluster.py
+++ tests/databases/redis/test_cluster.py
@@ -1,12 +1,7 @@
 import pytest
-import time
-
 import redis as redisdb
 
 from testsuite.databases.redis import service
-
-
-CLUSTER_INIT_TIMEOUT = 5.0
 
 
 @pytest.fixture(scope='session')
@@ -15,21 +10,13 @@ def _redis_service_settings(pytestconfig):
 
 
 def test_cluster_basic(redis_service, _redis_service_settings: service.ServiceSettings):
-    def cluster_nodes():
-        redis_db = redisdb.StrictRedis(
-            host=_redis_service_settings.host,
-            port=_redis_service_settings.master_ports[0],
-        )
-        return redis_db.cluster('NODES')
+    redis_db = redisdb.StrictRedis(
+        host=_redis_service_settings.host,
+        port=_redis_service_settings.master_ports[0],
+    )
+    cluster_nodes = redis_db.cluster('NODES')
 
-    begin = time.time()
-    while time.time() - begin < CLUSTER_INIT_TIMEOUT:
-        slaves = [n for _, n in cluster_nodes().items() if 'slave' in n['flags']]
-        if len(slaves) == len(_redis_service_settings.slave_ports):
-            break
-    assert time.time() - begin < CLUSTER_INIT_TIMEOUT
-
-    for node, info in cluster_nodes().items():
+    for node, info in cluster_nodes.items():
         port = int(node.rsplit(':', maxsplit=1)[-1])
         if 'master' in info['flags']:
             assert port in _redis_service_settings.master_ports

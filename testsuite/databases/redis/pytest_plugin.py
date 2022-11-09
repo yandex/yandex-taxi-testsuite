06a1acfdfc70fb7a648004bbb3616e6d67bf43b5
Aleksandr Lazarev <lexlazarev@userver.tech>
2022-11-09 15:15:13 +0300

    implement connection cache

--- testsuite/databases/redis/pytest_plugin.py
+++ testsuite/databases/redis/pytest_plugin.py
@@ -51,35 +51,57 @@ def redis_service(
         ensure_service_started(service_name, settings=_redis_service_settings)
 
 
-@pytest.fixture
-def redisdb(redis_service, _redis_service_settings):
-    params = {
-        'host': _redis_service_settings.host,
-        'port': _redis_service_settings.master_ports[0]
-    }
-    if _redis_service_settings.cluster_mode:
-        conn = redis.RedisCluster(
-            host=_redis_service_settings.host,
-            port=_redis_service_settings.master_ports[0],
-        )
-    else:
-        conn = redis.StrictRedis(
-            host=_redis_service_settings.host,
-            port=_redis_service_settings.master_ports[0],
-        )
-    assert conn.ping()
-    return conn
+@pytest.fixture(scope='session')
+def _redis_connection_cache(redis_service):
+    redis_cluster = None
+    redis_db = None
+
+    def get_connection(settings: service.ServiceSettings):
+        nonlocal redis_cluster
+        nonlocal redis_db
+
+        if settings.cluster_mode:
+            if redis_cluster is None:
+                redis_cluster = redis.RedisCluster(
+                    host=settings.host,
+                    port=settings.master_ports[0],
+                )
+            return redis_cluster
+        if redis_db is None:
+            redis_db = redis.StrictRedis(
+                host=settings.host,
+                port=settings.master_ports[0],
+            )
+        return redis_db
+
+    return get_connection
 
 
 @pytest.fixture
-def redisdb_sentinel(redis_service, _redis_service_settings):
+def redisdb(_redis_connection_cache, _redis_service_settings):
+    return _redis_connection_cache(_redis_service_settings)
+
+
+@pytest.fixture(scope='session')
+def _redis_sentinel_connection_cache(redis_service):
+    redis_sentinel = None
+
+    def get_connection(settings: service.ServiceSettings):
+        nonlocal redis_sentinel
+        if redis_sentinel is None:
+            redis_sentinel = redis.StrictRedis(
+                host=settings.host,
+                port=settings.sentinel_port,
+            )
+        return redis_sentinel
+
+    return get_connection
+
+
+@pytest.fixture
+def redisdb_sentinel(_redis_sentinel_connection_cache, _redis_service_settings):
     assert not _redis_service_settings.cluster_mode
-    conn = redis.StrictRedis(
-        host=_redis_service_settings.host,
-        port=_redis_service_settings.sentinel_port,
-    )
-    assert conn.ping()
-    return conn
+    return _redis_sentinel_connection_cache(_redis_service_settings)
 
 
 @pytest.fixture

a55d8670cbe82e38f35512dc0baa63120b2ed595
Vasily Kulikov <segoon@yandex-team.ru>
2022-11-21 19:06:15 +0300

    add test

--- tests/test_http.py
+++ tests/test_http.py
@@ -1,3 +1,4 @@
+import aiohttp
 from testsuite.utils import http
 
 
@@ -23,3 +24,46 @@ def test_make_response_json():
     response = http.make_response(json={'foo': 'bar'})
     assert response.body == b'{"foo": "bar"}'
     assert response.content_type == 'application/json'
+
+
+async def test_multipart_form_data(mockserver):
+    with aiohttp.MultipartWriter('form-data') as data:
+        payload = aiohttp.payload.StringPayload('some_app_id')
+        payload.set_content_disposition('form-data', name='application_id')
+        data.append_payload(payload)
+
+        payload = aiohttp.payload.BytesPayload(
+            b'image_data', headers={'Content-Type': 'image/jpeg'},
+        )
+        payload.set_content_disposition('form-data', name='passport_photo')
+        data.append_payload(payload)
+
+        payload = aiohttp.payload.StringPayload('42')
+        payload.set_content_disposition('form-data', name='page_number')
+        data.append_payload(payload)
+
+    headers = {
+        'Content-Type': 'multipart/form-data; boundary=' + data.boundary,
+    }
+
+    @mockserver.handler('multipart/form-data')
+    def mock(request):
+        assert request.headers['content-type'].startswith(
+            'multipart/form-data;',
+        )
+
+        form = request.form
+        assert form['application_id'] == 'some_app_id'
+        assert form['passport_photo'] == 'image_data'
+        assert form['page_number'] == 42
+        assert len(form) == 3
+
+        return mockserver.make_response()
+
+    async with aiohttp.ClientSession() as session:
+        response = await session.post(
+            mockserver.url('multipart/form-data'), headers=headers, data=data,
+        )
+
+    assert response.status == 200
+    assert mock.times_called == 1

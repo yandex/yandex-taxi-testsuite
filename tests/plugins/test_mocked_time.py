7fa47cf24757becf31f124500c4ad1c2ae90346a
Andrey Sazikov <sazikov.ad@phystech.edu>
2024-03-01 11:44:02 +0300

    add more tests

--- tests/plugins/test_mocked_time.py
+++ tests/plugins/test_mocked_time.py
@@ -15,9 +15,20 @@ def test_mocked_time_mutators(mocked_time):
     mocked_time.set(datetime.datetime(2021, 1, 1, 0, 0, 0))
     assert mocked_time.now() == datetime.datetime(2021, 1, 1, 0, 0, 0)
 
-    assert mocked_time.now(
-        tz=datetime.timezone(datetime.timedelta(hours=3), name='UTC+3')
-    ).replace(tzinfo=None) == datetime.datetime(2021, 1, 1, 3, 0, 0)
+
+@pytest.mark.now('2020-01-01T05:00:00.000+0430')
+def test_mocked_time_timezone_conversion(mocked_time):
+    msk_tz = datetime.timezone(datetime.timedelta(hours=3), name='UTC+3')
+
+    assert mocked_time.now() == datetime.datetime(2020, 1, 1, 0, 30, 0)
+
+    mocked_time.set(datetime.datetime(2021, 1, 1, 3, 0, 0, tzinfo=msk_tz))
+
+    assert mocked_time.now() == datetime.datetime(2021, 1, 1, 0, 0, 0)
+
+    assert mocked_time.now(tz=msk_tz) == datetime.datetime(
+        2021, 1, 1, 0, 0, 0, tzinfo=datetime.timezone.utc
+    )
 
 
 @pytest.mark.now(enabled=False)

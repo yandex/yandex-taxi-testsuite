b1f3676aa92d7ba062b31b9d1eb2e7a73bb3ba6e
Antony Polukhin <antoshkka@gmail.com>
2024-03-10 21:31:05 +0300

    fix

--- testsuite/plugins/network.py
+++ testsuite/plugins/network.py
@@ -1,4 +1,5 @@
 import contextlib
+import errno
 import itertools
 import socket
 import typing
@@ -36,18 +37,22 @@ def get_free_port() -> typing.Callable[[], int]:
         nonlocal sock_list
 
         sock = socket.socket(socket_af, socket.SOCK_STREAM)
-        with contextlib.closing(sock):
-            addr = ('127.0.0.1' if socket_af == socket.AF_INET else '::', 0)
-            try:
-                sock.bind(addr)
-                sock_list.append(sock)
-                return sock.getsockname()[1]
-            except OSError as err:
-                if socket_af != socket.AF_INET:
-                    if err.errno == errno.EADDRNOTAVAIL:
-                        socket_af = socket.AF_INET
-                        return _get_free_port()
+        addr = ('127.0.0.1' if socket_af == socket.AF_INET else '::', 0)
+        try:
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            sock.bind(addr)
+            sock_list.append(sock)
+            return sock.getsockname()[1]
+        except OSError as err:
+            if socket_af != socket.AF_INET:
+                if err.errno == errno.EADDRNOTAVAIL:
+                    socket_af = socket.AF_INET
+                    return _get_free_port()
 
         raise NoEnabledPorts()
 
-    return _get_free_port
+    try:
+        yield _get_free_port
+    finally:
+        for sock in sock_list:
+            sock.close()

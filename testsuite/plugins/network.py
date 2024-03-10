100088ff88b088e4da5705a6e2962a40d3e41ff5
Antony Polukhin <antoshkka@gmail.com>
2024-03-10 23:48:25 +0300

    fix

--- testsuite/plugins/network.py
+++ testsuite/plugins/network.py
@@ -16,7 +16,8 @@ class NoEnabledPorts(BaseError):
     """Raised if there are not free ports for worker"""
 
 
-def _get_ipv6_localhost_or_fallback() -> str:
+@pytest.fixture(scope='session')
+def _get_ipv6_af_or_fallback():
     if not hasattr(socket, 'AF_INET6'):
         return socket.AF_INET
 
@@ -30,14 +31,11 @@ def _get_ipv6_localhost_or_fallback() -> str:
     return socket.AF_INET
 
 
-_IPV6_AF_OR_FALLBACK = _get_ipv6_localhost_or_fallback()
-_LOCALHOST = '127.0.0.1' if _IPV6_AF_OR_FALLBACK == socket.AF_INET else '::'
 
-
-def _is_port_free(port_num: int) -> bool:
-    sock = socket.socket(_IPV6_AF_OR_FALLBACK, socket.SOCK_STREAM)
+def _is_port_free(port_num: int, socket_af, host: str) -> bool:
+    sock = socket.socket(socket_af, socket.SOCK_STREAM)
     try:
-        sock.bind((_LOCALHOST, port_num))
+        sock.bind((host, port_num))
         return True
     finally:
         sock.close()
@@ -45,30 +43,33 @@ def _is_port_free(port_num: int) -> bool:
     return False
 
 
-async def _get_free_port_sock_storing() -> typing.Callable[[], int]:
-    # Relies on https://github.com/torvalds/linux/commit/aacd9289af8b82f5fb01b
+async def _get_open_sock_list_impl():
     sock_list = set()
-
-    def _get_free_port():
-        nonlocal sock_list
-
-        sock = socket.socket(_IPV6_AF_OR_FALLBACK, socket.SOCK_STREAM)
-        try:
-            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            sock.bind((_LOCALHOST, 0))
-            sock_list.add(sock)
-            return sock.getsockname()[1]
-        except OSError:
-            raise NoEnabledPorts()
-
     try:
-        yield _get_free_port
+        yield sock_list
     finally:
         for sock in sock_list:
             sock.close()
 
 
-async def _get_free_port_range_based() -> typing.Callable[[], int]:
+def _get_free_port_sock_storing(socket_af, host: str, sock_list: set) -> typing.Callable[[], int]:
+    # Relies on https://github.com/torvalds/linux/commit/aacd9289af8b82f5fb01b
+    def _get_free_port():
+        nonlocal sock_list
+
+        sock = socket.socket(socket_af, socket.SOCK_STREAM)
+        try:
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            sock.bind((host, 0))
+            sock_list.add(sock)
+            return sock.getsockname()[1]
+        except OSError:
+            raise NoEnabledPorts()
+
+
+async def _get_free_port_range_based(socket_af, host: str)
+    -> typing.Callable[[], int]
+:
     port = 61000
 
     def _get_free_port():
@@ -78,20 +79,24 @@ async def _get_free_port_range_based() -> typing.Callable[[], int]:
         while port > close_to_privileged_ports:
             port -= 1
 
-            if _is_port_free(port):
+            if _is_port_free(port, socket_af, host):
                 return port
 
         raise NoEnabledPorts()
 
-    yield _get_free_port
+    return _get_free_port
 
 
 @pytest.fixture(scope='session')
-async def get_free_port() -> typing.Callable[[], int]:
+def get_free_port(_get_ipv6_af_or_fallback, _get_localhost, _get_open_sock_list_impl)
+    -> typing.Callable[[], int]
+:
     """
     Returns an ephemeral TCP port that is free for IPv4 and for IPv6.
     """
     if platform.system() == 'Linux':
-        yield _get_free_port_sock_storing()
+        return _get_free_port_sock_storing(
+            _get_ipv6_af_or_fallback, _get_localhost, _get_open_sock_list_impl,
+        )
 
-    yield _get_free_port_range_based()
+    return _get_free_port_range_based(_get_ipv6_af_or_fallback, _get_localhost)

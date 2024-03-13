43fb2f4ea7b8c938160fcec9c71bd6afe9ea07b3
Antony Polukhin <antoshkka@gmail.com>
2024-03-13 14:12:18 +0300

    Reliable get_free_port() (#80)

--- tests/plugins/test_network.py
+++ tests/plugins/test_network.py
@@ -5,6 +5,9 @@ from testsuite.plugins import network
 
 @pytest.mark.nofilldb
 def test_get_free_port_error(get_free_port):
-    with pytest.raises(network.NoEnabledPorts):
-        for _ in range(network.MAX_PORTS_NUMBER + 1):
-            get_free_port()
+    max_ports = 101
+
+    ports = set()
+    for _ in range(max_ports):
+        ports.add(get_free_port())
+    assert len(ports) == max_ports, 'get_free_port() returns duplicate ports'
--- testsuite/plugins/network.py
+++ testsuite/plugins/network.py
@@ -1,12 +1,11 @@
 import contextlib
+import errno
 import itertools
+import platform
 import socket
 import typing
 
 import pytest
-
-BASE_PORT = 30000
-MAX_PORTS_NUMBER = 100
 
 
 class BaseError(Exception):
@@ -18,18 +17,102 @@ class NoEnabledPorts(BaseError):
 
 
 @pytest.fixture(scope='session')
-def get_free_port(worker_id: str) -> typing.Callable[[], int]:
-    counter = itertools.islice(itertools.count(), MAX_PORTS_NUMBER)
-    worker_num = 0 if worker_id == 'master' else int(worker_id[2:]) + 1
+def _get_ipv6_af_or_fallback():
+    if not hasattr(socket, 'AF_INET6'):
+        return socket.AF_INET
+
+    sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+    with contextlib.closing(sock):
+        try:
+            sock.bind(('::', 0))
+            return socket.AF_INET6
+        except OSError:
+            pass
+
+    return socket.AF_INET
+
+
+@pytest.fixture(scope='session')
+def _get_ipv6_localhost_or_fallback(_get_ipv6_af_or_fallback):
+    if _get_ipv6_af_or_fallback == socket.AF_INET:
+        return '127.0.0.1'
+
+    return '::'
+
+
+def _is_port_free(port_num: int, socket_af: int, host: str) -> bool:
+    sock = socket.socket(socket_af, socket.SOCK_STREAM)
+    with contextlib.closing(sock):
+        sock.bind((host, port_num))
+        return True
+
+    return False
+
+
+@pytest.fixture(scope='session')
+async def _get_open_sock_list_impl():
+    sock_list = []
+    try:
+        yield sock_list
+    finally:
+        for sock in sock_list:
+            sock.close()
+
+
+def _get_free_port_sock_storing(
+    socket_af: int,
+    host: str,
+    sock_list: list,
+) -> typing.Callable[[], int]:
+    # Relies on https://github.com/torvalds/linux/commit/aacd9289af8b82f5fb01b
+    def _get_free_port():
+        sock = socket.socket(socket_af, socket.SOCK_STREAM)
+        try:
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            sock.bind((host, 0))
+            sock_list.append(sock)  # shared variable
+            return sock.getsockname()[1]
+        except OSError:
+            raise NoEnabledPorts()
+
+    return _get_free_port
+
+
+def _get_free_port_range_based(
+    socket_af: int,
+    host: str,
+) -> typing.Callable[[], int]:
+    port_seq = itertools.count(61000, -1)
 
     def _get_free_port():
-        for value in counter:
-            port = BASE_PORT + worker_num * MAX_PORTS_NUMBER + value
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            with contextlib.closing(sock):
-                result_code = sock.connect_ex(('localhost', port))
-            if result_code != 0:
+        close_to_privileged_ports = 2048
+        port = next(port_seq)
+        while port > close_to_privileged_ports:
+            if _is_port_free(port, socket_af, host):
                 return port
+
         raise NoEnabledPorts()
 
     return _get_free_port
+
+
+@pytest.fixture(scope='session')
+def get_free_port(
+    _get_ipv6_af_or_fallback,
+    _get_ipv6_localhost_or_fallback,
+    _get_open_sock_list_impl,
+) -> typing.Callable[[], int]:
+    """
+    Returns an ephemeral TCP port that is free for IPv4 and for IPv6.
+    """
+    if platform.system() == 'Linux':
+        return _get_free_port_sock_storing(
+            _get_ipv6_af_or_fallback,
+            _get_ipv6_localhost_or_fallback,
+            _get_open_sock_list_impl,
+        )
+
+    return _get_free_port_range_based(
+        _get_ipv6_af_or_fallback,
+        _get_ipv6_localhost_or_fallback,
+    )

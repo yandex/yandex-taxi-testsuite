a843e2803f62df0a5bbd5fc7d0a64b8c3a5d6f0e
Victor Makarov <vitja@yandex-team.ru>
2024-03-19 19:59:24 +0300

    Update docs version 0.1.34

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.30',
+    VERSION: '0.1.34',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'dirhtml',

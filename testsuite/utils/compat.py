87262945580069843fb2a9f1ca5207a92c7ab832
Andrey Sazikov <asazikov2017@yandex.ru>
2025-02-11 10:05:49 +0300

    [WIP] add mypy checker

--- testsuite/utils/compat.py
+++ testsuite/utils/compat.py
@@ -3,7 +3,8 @@ import contextlib
 
 # Required for python3.6 compatibility
 if not hasattr(contextlib, 'asynccontextmanager'):
-    import contextlib2  # pylint: disable=import-error
+    # pylint: disable=import-error 
+    import contextlib2  # type: ignore
 
     asynccontextmanager = contextlib2.asynccontextmanager
 else:

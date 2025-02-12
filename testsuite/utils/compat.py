275e52d13f420672ddd875c4fcfd74dec672cdc4
Andrey Sazikov <asazikov2017@yandex.ru>
2025-02-12 16:19:46 +0300

    revert utils/compat.py

--- testsuite/utils/compat.py
+++ testsuite/utils/compat.py
@@ -1,6 +1,16 @@
 import contextlib
 
 
+# Required for python3.6 compatibility in https://github.com/userver-framework/userver
+if not hasattr(contextlib, 'asynccontextmanager'):
+    # pylint: disable=import-error
+    import contextlib2  # type: ignore
+
+    asynccontextmanager = contextlib2.asynccontextmanager
+else:
+    asynccontextmanager = contextlib.asynccontextmanager
+
+
 if not hasattr(contextlib, 'aclosing'):
 
     @contextlib.asynccontextmanager

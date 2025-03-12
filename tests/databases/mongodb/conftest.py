7663bc1b97bb0a25d6b853c67d67b801ae7bd828
Victor Makarov <vitja@yandex-team.ru>
2025-03-12 14:51:16 +0300

    fix

--- tests/databases/mongodb/conftest.py
+++ tests/databases/mongodb/conftest.py
@@ -7,6 +7,7 @@ import pytest
 def schemas_directory() -> pathlib.Path:
     return pathlib.Path(__file__).parent / 'schemas'
 
+
 @pytest.fixture(scope='session')
 def mongo_schema_directory(schemas_directory):
-    return  schemas_directory / 'mongo'
+    return schemas_directory / 'mongo'

2db1eae982c63bb0e2573cf7d274629042f98dfa
Andrey Sazikov <asazikov2017@yandex.ru>
2025-03-28 15:15:36 +0300

    better

--- testsuite/utils/subprocess_helper.py
+++ testsuite/utils/subprocess_helper.py
@@ -2,10 +2,7 @@ import subprocess
 import typing
 
 
-def sh(
-    *args: str,
-    nostderr: bool = True
-) -> str:  # pylint: disable=invalid-name
+def sh(*args: str, nostderr: bool = True) -> str:  # pylint: disable=invalid-name
     stderr: typing.Optional[int]
     if nostderr:
         stderr = subprocess.DEVNULL

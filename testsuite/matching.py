a3581c8bc44404115842ae4391f45309bb9942f3
Andrey Sazikov <asazikov2017@yandex.ru>
2025-06-03 15:11:14 +0300

    Update matching.py

--- testsuite/matching.py
+++ testsuite/matching.py
@@ -218,7 +218,7 @@ class Not:
 
 
 class Comparator:
-    op: typing.Callable[[typing.Any, typing.Any], bool] = operator.eq
+    op: typing.Callable[[typing.Any, typing.Any], typing.Any] = operator.eq
 
     def __init__(self, value):
         self._value = value

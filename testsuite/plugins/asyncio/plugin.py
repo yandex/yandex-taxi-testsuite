834a38de8e7b67351214e7040f9a9d461756f2fb
Andrey Sazikov <asazikov2017@yandex.ru>
2025-11-11 13:02:45 +0300

    fix

--- testsuite/plugins/asyncio/plugin.py
+++ testsuite/plugins/asyncio/plugin.py
@@ -1,10 +1,12 @@
+import pytest
+
+
+@pytest.hookimpl(tryfirst=True)
 def pytest_configure(config):
     # Force default asyncio mode
     config.option.asyncio_mode = 'auto'
     # Force fixtures to use session loop
-    config.addinivalue_line(
-        'addopts', 'asyncio_default_fixture_loop_scope = session'
-    )
+    config._inicache['asyncio_default_fixture_loop_scope'] = 'session'
 
 
 def pytest_collection_modifyitems(items):

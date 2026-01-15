18371ccd122a09ab2c6e7d7284a5f879b0eaf154
Victor Makarov <vitja.makarov@gmail.com>
2026-01-15 18:36:08 +0300

    fix

--- testsuite/mockserver/server.py
+++ testsuite/mockserver/server.py
@@ -283,7 +283,7 @@ class Server:
     def server_info(self) -> classes.MockserverInfo:
         return self._info
 
-    def get_debug(self):
+    def get_debug(self) -> bool:
         return self._mockserver_debug
 
     def set_debug(self, enabled: bool):

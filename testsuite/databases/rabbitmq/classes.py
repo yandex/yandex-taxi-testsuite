3b7f525aea62f26af7b7b598f3574333fd1d54a7
Andrey Sazikov <asazikov2017@yandex.ru>
2026-05-21 14:59:48 +0300

    fix ci: update kafka package and ports for rabbit

--- testsuite/databases/rabbitmq/classes.py
+++ testsuite/databases/rabbitmq/classes.py
@@ -44,8 +44,17 @@ class Channel:
             timeout=timeout,
         )
 
-    async def declare_queue(self, queue: str, timeout: float = 1.0) -> None:
-        await self._channel.declare_queue(name=queue, timeout=timeout)
+    async def declare_queue(
+        self,
+        queue: str,
+        durable: bool = True,
+        timeout: float = 1.0,
+    ) -> None:
+        await self._channel.declare_queue(
+            name=queue,
+            durable=durable,
+            timeout=timeout,
+        )
 
     async def bind_queue(
         self,
